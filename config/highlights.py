71589b670897498325ac46b7db99904d9581fb26
sofiapinto <anasbpinto3@gmail.com>
2025-07-23 12:34:51 +0100

    profile specific highlights

---
+++ config/highlights.py
@@ -0,0 +1,16 @@
+"""
+Profile specific manually curated highlights.
+"""
+
+highlights = {
+    1: "This profile reflects the highest number of households (25%). This is also the oldest group with 40% aged 65+ only, and a great % not on work. Across other characteristics, households in profile 1 are average.",
+    2: "Profile 2 has the lowest electricity consumption, and has the greatest number of social housing tenants. This profile sees the gretest proportion of single occupancy households, and the least with children.",
+    3: "Profile 3 has second highest electricity usage, and the highest proportion of households where everyone is in work, education, or training.",
+    4: "",
+    5: "",
+    6: "",
+    7: "",
+    8: "",
+    9: "",
+    10: "",
+}
