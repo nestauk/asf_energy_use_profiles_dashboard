10c9af99022fe85524ed2d2996733703766683a3
sofiapinto <anasbpinto3@gmail.com>
2025-07-23 20:11:09 +0100

    add documentation to configs.py

--- config/configs.py
+++ config/configs.py
@@ -1,2 +1,5 @@
-households_low_gas_count = [7, 10]
+# profiles with low count of households on gas
+profiles_low_gas_count = [7, 10]
+
+# profile numbers
 profile_numbers = [i for i in range(1, 11)]
